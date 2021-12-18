4842c2ab12e19a719a01aa73c65fcecd863ef456
yassineazrak <yassineazrak2019@gmail.com>
2021-12-18 21:05:35 +0100

    part3 not work

--- part3/init.sh
+++ part3/init.sh
@@ -7,3 +7,7 @@ yum -y remove containers-common
 
 ### install kubctl
 #https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
+kubectl port-forward redis-master-765d459796-258hz 7000:6379
+
+# ssh -- -L 31526:localhost:31526
+# vagrant ssh -- -L 31526:localhost:31526 
