27c7e9eaa18d726a6b2428d6c50d140c23f6e21f
yassineazrak <yassineazrak2019@gmail.com>
2021-12-18 17:20:34 +0100

    part3 not work

---
+++ part3/init.sh
@@ -0,0 +1,9 @@
+yum -y remove podman
+yum -y remove containers-common
+
+
+###install docker 
+# https://docs.docker.com/engine/install/centos/
+
+### install kubctl
+#https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
