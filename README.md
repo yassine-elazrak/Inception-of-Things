3bd60572057fc26bbb8b0d527c052939963353cf
yassineazrak <yassineazrak2019@gmail.com>
2022-02-12 21:52:48 +0100

    correct p1

--- README.md
+++
@@ -1,2 +0,0 @@
-# Inception-of-Things
-This project aims to deepen your knowledge by making you use K3d and K3s with Vagrant.
---
+++ p2/scripts/install.sh
@@ -0,0 +1,5 @@
+kubectl apply -f  deploy.yaml
+kubectl apply -f  service.yaml 
+kubectl apply -f  ingress.yaml
+
+ kubectl wait --for=condition=Ready pods --all -n argocd
