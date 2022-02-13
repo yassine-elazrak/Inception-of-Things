4ff0eec8e8ae2896001843497a51d7db31ff4cc7
yassineazrak <yassineazrak2019@gmail.com>
2022-02-13 16:39:08 +0100

    docker fail

--- p2/scripts/install.sh
+++ p2/scripts/install.sh
@@ -2,4 +2,4 @@ kubectl apply -f  deploy.yaml
 kubectl apply -f  service.yaml 
 kubectl apply -f  ingress.yaml
 
- kubectl wait --for=condition=Ready pods --all -n argocd
+kubectl wait --for=condition=Ready pods --all 
